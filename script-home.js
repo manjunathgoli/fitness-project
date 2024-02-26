77710020bb433b0a62d2115d7a30661b1c0dace6
manjunathgoli <158014117+manjunathgoli@users.noreply.github.com>
2024-02-26 19:08:42 +0530

    Update script-home.js

--- script-home.js
+++ script-home.js
@@ -225,3 +225,55 @@ function showClaves(){
     isShow_Claves = true;
     }
 }
+let available=[
+    'shoulders',
+    'biceps',
+    'triceps',
+    'forearm',
+    'upper chest',
+    'lower Chest',
+    'inner chest',
+    'middle chest',
+    'trapezius',
+    'lats',
+    'rhomboids',
+    'lower back',
+    'upper abs',
+    'lower abs',
+    'quads',
+    'hamstrings',
+    'glutes',
+    'claves',
+];
+
+const resultsBox = document.querySelector(".result-box");
+const inputBox= document.getElementById("input-box");
+
+inputBox.onkeyup = function(){
+    let result=[];
+    let input = inputBox.value;
+    
+    if(input.length){
+        result = available.filter((keyword)=>{
+           return keyword.toLowerCase().includes(input.toLowerCase());
+
+        });
+        console.log(result);
+    }
+    display(result)
+    if(!result.length){
+        resultsBox.innerHTML='';
+    }
+}
+function display(result){
+    const content = result.map((list)=>{
+        return "<li onclick=selectInput(this)>" + list + "</li>"
+
+    });
+
+    resultsBox.innerHTML = "<ul>" + content.join('') + "</ul>";
+}
+function selectInput(list){
+    inputBox.value = list.innerHTML;
+    resultsBox.innerHTML = '';
+}
