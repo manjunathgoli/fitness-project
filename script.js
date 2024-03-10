1435c161f16e542285b15f56eeb787148ef6e723
manjunathgoli <158014117+manjunathgoli@users.noreply.github.com>
2024-03-10 21:33:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,21 @@ btnPopup.addEventListener('click',()=>{
 iconClose.addEventListener('click',()=>{
     wrapper.classList.remove('active-popup');
 });
+var swiper = new Swiper(".slide-container", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    centerSlide: "true",
+    grabCursor: "true",
+    fade: "true",
+    pagination:{
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
